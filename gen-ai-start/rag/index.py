e47dbe40595b28e4469508a1a4e8cd688c6dec05
Rinkesh375 <rinkeshujjwal16@gmail.com>
2026-09-01 21:31:45 +0530

    updated requirements file

---
+++ gen-ai-start/rag/index.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+from langchain_community.document_loaders import PyPDFLoader
+
+
+pdf_path = Path(__file__).parent / "nodejs.pdf"
+
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()
+
+print(docs[2])
